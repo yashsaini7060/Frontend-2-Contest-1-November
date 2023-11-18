d7d3a9bf65fa3f5db2dabb5e587a6147bfa13e06
yashsaini7060 <yk18052000@gmail.com>
2023-11-18 12:40:32 +0530

    added few logs

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const data = [
 
 // 1. Print Developers
 function printDeveloper() {
+  console.log("Developers :")
   const developers = data.forEach(person => {
     if(person.profession.toLowerCase() === "developer"){
       console.log(person)
@@ -19,14 +20,14 @@ function addData() {
   const profession = prompt("Enter Profession:");
   const newObj = {name, age, profession};
   data.push(newObj);
-  console.log("Updated Array");
+  console.log("Updated Array :");
   console.log(data);
 }
 
 // 3. Remove Admins
 function removeAdmin() {
   const fileredData = data.filter(person => person.profession.toLowerCase() !== "admin")
-  console.log("Filtered Data");
+  console.log("Filtered Data :");
   console.log(fileredData);
 }
 
@@ -39,6 +40,7 @@ function concatenateArray() {
     { name: "Utkarsh sharma", age: 21, profession: "developer" },
   ]
   const newArray = data.concat(array1);
+  console.log("Concatenated Array :")
   console.log(newArray);
 }
 
@@ -51,12 +53,14 @@ function averageAge() {
     i++;
   }
   const averageAge = sumOfAge/data.length;
+  console.log("Average Age :");
   console.log(averageAge);
 }
 
 // 6. Age Check
 function checkAgeAbove25() {
   const isAbove25 = data.filter(person => person.age> 25)
+  console.log("People above 25 :");
   console.log(isAbove25);
 }
 
@@ -64,20 +68,22 @@ function checkAgeAbove25() {
 function uniqueProfessions() {
   const professions = data.map(person => person.profession);
   const uniqueProfessionsSet = new Set(professions);
-  console.log("Unique professions:");
+  console.log("Unique professions :");
   console.log(Array.from(uniqueProfessionsSet));
 }
 
 // 8. Sort by Age
 function sortByAge() {
   const sortedArray = data.sort((a, b) => a.age - b.age);
-  console.log("Sorted Array by Age:", sortedArray);
+  console.log("Sorted Array by Age :");
+  console.log(sortedArray)
 }
 
 // 9. Update Profession
 function updateJohnsProfession() {
   const index = data.findIndex(person=> person.name.toLowerCase() === "john");
   data[index].profession = "manager";
+  console.log("Updated John Profession :")
   console.log(data);
 }
 
@@ -91,6 +97,6 @@ function getTotalProfessions() {
     else if(person.profession.toLowerCase() === "admin")
       adminsCount++;
   })
-  console.log("Developers Count:", developersCount);
-  console.log("Admins: Count", adminsCount);
+  console.log("Developers Count :", developersCount);
+  console.log("Admins Count :", adminsCount);
 }
